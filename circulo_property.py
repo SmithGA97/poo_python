121b50921a0df0d024bf2c6365f2118dfdfebc1d
Brayan <smithgaspri@gmail.com>
2021-09-23 10:11:23 -0500

    circulo y prefijos

---
+++ circulo_property.py
@@ -0,0 +1,30 @@
+class Circulo:
+    
+    pi = 3.141592
+    
+    def __init__(self, radio):
+        self.__radio = radio
+    
+    @property
+    def cuadrado(self, n):
+        return n ** 2
+    
+    @cuadrado.setter
+    def area(self):
+        return Circulo.pi * self.cuadrado(self.radio)
+    
+    def valorRadio(self):
+        return self.radio
+    
+    def fijaRadio(self, nuevoRadio):
+        self.radio = nuevoRadio
+
+c = Circulo(3)
+print(c.area())
+print(c.valorRadio())
+c.fijaRadio(4)
+print(c.valorRadio())
+print(c.area())
+#intentando modificar el radio para que cambiar areea
+c.radio=10
+print(c.area())
